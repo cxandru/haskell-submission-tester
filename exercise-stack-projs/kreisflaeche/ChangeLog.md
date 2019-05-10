55ce10fa9ff6fef8a5ee53fd953af4ba15cc6e48
Gregor Cassian Alexandru <gregor.alexandru@campus.lmu.de>
2019-05-10 22:45:24 +0200

    removed specific exercise stack projects

--- exercise-stack-projs/kreisflaeche/ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for submission-test-template
-
-## Unreleased changes
--- exercise-stack-projs/kreisflaeche/src/L.hs
+++
@@ -1,9 +0,0 @@
-module L (punkteImKreis, anteilImKreis)
-where
-
-punkteImKreis :: Double -> [( Double , Double ) ]
-punkteImKreis k =
-  [( x , y ) | x <- [1.. k ] , y <- [1.. k ] , sqrt ( x ^2 + y ^2) < k ]
-anteilImKreis :: Double -> Double
-anteilImKreis k =
-  ( fromIntegral ( length ( punkteImKreis k ) ) ) / ( k * k )
