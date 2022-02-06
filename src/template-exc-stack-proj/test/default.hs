aaab14b9f288eb5227062f1bb4a446ce83513b5d
G. Cassian Alexandru <galexandru@lilo7.science.ru.nl>
2022-02-06 19:40:52 +0100

    Minor changes to template proj
    Clean up yaml format (- -Wall instead of -Wall)
    Include default Lang Exts
    import HU in `default.hs`
    comment out `a` target to avoid errors when unused

--- src/template-exc-stack-proj/test/default.hs
+++ src/template-exc-stack-proj/test/default.hs
@@ -1,6 +1,7 @@
 module Main where
 import Test.Tasty (testGroup, defaultMainWithIngredients, TestTree, localOption, )
 import Test.Tasty.QuickCheck as QC
+import Test.Tasty.HUnit as HU
 import Test.Tasty.Runners.AntXML (antXMLRunner)
 import qualified L (foo)
 import S (foo)
@@ -19,4 +20,6 @@ tests :: TestTree
 tests = testGroup "{S}: foo"
   [ QC.testProperty "Quickcheck" $
       \a -> on (===) ($ a) S.foo L.foo
+  , HU.testCase "HUnit" $
+    S.foo undefined @?= undefined
   ]
